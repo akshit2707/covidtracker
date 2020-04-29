107ae1df49bbfaaaabeb7a60884443202c4fd676
akshit2707 <akshit2707.iitkgp@gmail.com>
2020-04-29 18:01:12 +0530

    step 1 adding fucbtons

--- covid.py
+++ covid.py
@@ -1,4 +1,32 @@
+#importing libraries
+
 import requests
 import bs4
 import plyer
+import tkinter as tk
+import time 
+import datetime
+
+
+# defininfg functions
+def get_data(url):
+
+	data = requests.get(url)
+	return data
+
+
+def get_corona_details_india():
+	url = "https://www.mohfw.gov.in/"
+
+	html_view = get_data(url)
+
+	print(html_view.text)
+
+
+
+get_corona_details_india()
+
+
+
+
 
